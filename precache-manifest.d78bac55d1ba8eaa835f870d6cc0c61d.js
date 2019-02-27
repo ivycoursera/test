3151ff41e03533333d58345027766c79650e3914
Vi <liliput_j@yahoo.com>
2019-02-27 10:33:23 -0800

    Updates

---
+++ precache-manifest.d78bac55d1ba8eaa835f870d6cc0c61d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.a03dc12c.js"
+  },
+  {
+    "revision": "634432e3b7cade26adbf",
+    "url": "/test/static/js/main.634432e3.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "634432e3b7cade26adbf",
+    "url": "/test/static/css/main.5a482903.chunk.css"
+  },
+  {
+    "revision": "16ab88f88056c5cc0aa9f2254b55dbb8",
+    "url": "/test/index.html"
+  }
+];
